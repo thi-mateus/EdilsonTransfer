982982eb0292ae2cc35ae37ef0bea5e71fb2272c
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-11-13 09:53:07 -0300

    desfazendo alteracao databases

--- EdilsonTransfer/settings.py
+++ EdilsonTransfer/settings.py
@@ -78,24 +78,14 @@ WSGI_APPLICATION = 'EdilsonTransfer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         'TIME_ZONE': 'America/Sao_Paulo',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'TIME_ZONE': 'America/Sao_Paulo',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
