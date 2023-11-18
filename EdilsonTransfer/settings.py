29ea8eeb19f6c0ac300ba453ca1fb7458c503132
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-11-18 10:18:14 -0300

    modificancdo dotenv db

--- EdilsonTransfer/settings.py
+++ EdilsonTransfer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS: list[str] = ['*']
 
 
 # Application definition
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'EdilsonTransfer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TIME_ZONE': 'America/Sao_Paulo',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'basededados',
-#         'USER': 'edilsontransfer',
-#         'PASSWORD': 'edilsontransferadmin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'TIME_ZONE': 'America/Sao_Paulo',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        # 'USER': os.environ.get('DATABASE_USER'),
+        # 'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        # 'HOST': os.environ.get('DATABASE_HOST'),
+        # 'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
 
 
 # Password validation
