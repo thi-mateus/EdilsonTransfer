ba715eaf1ccf894850c42479b39138c09346d254
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-11-20 13:01:07 -0300

    settings e wsgi

--- EdilsonTransfer/wsgi.py
+++ EdilsonTransfer/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EdilsonTransfer.settings')
 
 load_dotenv()
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
