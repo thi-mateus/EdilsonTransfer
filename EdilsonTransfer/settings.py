60bbaed3e6bedcf5f62c5b14fb4a22546c119c5e
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-11-21 17:32:22 -0300

    settings.py

--- EdilsonTransfer/settings.py
+++ EdilsonTransfer/settings.py
@@ -90,10 +90,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE'),
         'NAME': os.environ.get('DATABASE_NAME'),
-        # 'USER': os.environ.get('DATABASE_USER'),
-        # 'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        # 'HOST': os.environ.get('DATABASE_HOST'),
-        # 'PORT': os.environ.get('DATABASE_PORT'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
